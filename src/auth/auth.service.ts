bfc798c26fadccec4b39bb2a260065682d356d97
Denis Kalkopf <deniskalkopf@gmail.com>
2024-02-28 20:37:47 +0500

    add authorization (wip)

---
+++ src/auth/auth.service.ts
@@ -0,0 +1,28 @@
+import { ForbiddenException, Injectable } from '@nestjs/common';
+import { CreateUserDto } from 'src/users/dto/create-user.dto';
+import { UsersService } from 'src/users/users.service';
+
+@Injectable()
+export class AuthService {
+  constructor(private userService: UsersService) {}
+
+  async validateUser(email: string, password: string): Promise<any> {
+    const user = await this.userService.findByEmail(email);
+
+    if (user && user.password === password) {
+      const { password, ...result } = user;
+      return result;
+    }
+
+    return null;
+  }
+
+  async register(dto: CreateUserDto) {
+    try {
+      const userData = await this.userService.create(dto);
+      return userData;
+    } catch (error) {
+      throw new ForbiddenException('Registration error');
+    }
+  }
+}
--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,5 +1,12 @@
+import { ApiProperty } from '@nestjs/swagger';
+
 export class CreateUserDto {
+  @ApiProperty({ default: 'test@test.com' })
   email: string;
+
+  @ApiProperty({ default: 'Test Name' })
   fullName: string;
+
+  @ApiProperty({ default: 1234 })
   password: string;
 }
