fa3f9c0d965c3ac019f6385b244506b06f7830f0
Denis Kalkopf <deniskalkopf@gmail.com>
2024-02-28 16:52:00 +0500

    connect db

--- src/files/entities/file.entity.ts
+++ src/files/entities/file.entity.ts
@@ -1,1 +1,32 @@
-export class File {}
+import { UserEntity } from 'src/users/entities/user.entity';
+import {
+  Entity,
+  PrimaryGeneratedColumn,
+  Column,
+  DeleteDateColumn,
+  ManyToOne,
+} from 'typeorm';
+
+@Entity('files')
+export class FileEntity {
+  @PrimaryGeneratedColumn()
+  id: number;
+
+  @Column()
+  filename: string;
+
+  @Column()
+  originalName: string;
+
+  @Column()
+  size: number;
+
+  @Column()
+  mimetype: number;
+
+  @ManyToOne(() => UserEntity, (user) => user.files)
+  user: UserEntity;
+
+  @DeleteDateColumn()
+  deletedAt: number;
+}
