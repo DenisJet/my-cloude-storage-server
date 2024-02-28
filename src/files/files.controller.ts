2da2687ee0274b2f43596bec598b992166ce4083
Denis Kalkopf <deniskalkopf@gmail.com>
2024-02-28 18:55:16 +0500

    add uploads path

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -8,6 +8,8 @@ import {
   Delete,
   UseInterceptors,
   UploadedFile,
+  ParseFilePipe,
+  MaxFileSizeValidator,
 } from '@nestjs/common';
 import { FilesService } from './files.service';
 import { CreateFileDto } from './dto/create-file.dto';
@@ -35,7 +37,14 @@ export class FilesController {
       },
     },
   })
-  create(@UploadedFile() file: Express.Multer.File) {
+  create(
+    @UploadedFile(
+      new ParseFilePipe({
+        validators: [new MaxFileSizeValidator({ maxSize: 1024 * 1024 * 1 })],
+      }),
+    )
+    file: Express.Multer.File,
+  ) {
     return file;
   }
 
